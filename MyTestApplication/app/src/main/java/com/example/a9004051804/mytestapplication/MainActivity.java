cf29bb604bbe15cab309ccbf6463fba1a2e0c763
Noguchi <Takuya.Noguchi@sony.com>
2018-07-05 19:42:08 +0900

    first

---
+++ MyTestApplication/app/src/main/java/com/example/a9004051804/mytestapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a9004051804.mytestapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
